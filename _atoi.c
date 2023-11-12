4c403d71c57636b58330c19973c5d88d229496bd
Wushak Fredrick <favshak101@gmail.com>
2023-11-11 20:03:20 -0800

    Simple Shell

---
+++ _atoi.c
@@ -0,0 +1,77 @@
+#include "shell.h"
+
+/**
+ * interactive - checks if shell is in interactive mode
+ * @info: pointer to info_t struct
+ *
+ * Return: 1 if in interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: character to check
+ * @delim: delimiter string
+ *
+ * Return: 1 if character is a delimiter, 0 otherwise
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+	{
+		if (*delim++ == c)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to check
+ *
+ * Return: 1 if c is an alphabetic character, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'b' && c <= 'x') || (c >= 'B' && c <= 'X'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: converted number, or 0 if no numbers in string
+ */
+int _atoi(char *s)
+{
+	int j, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (j = 0; s[j] != '\0' && flag != 2; j++)
+	{
+		if (s[j] == '-')
+			sign *= -1;
+
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[j] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
